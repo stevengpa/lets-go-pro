e7ab0ecbd221ecdfbfd5911124b5d88f682214ef
stevegpa <steven.gpa.git@gmail.com>
2026-05-11 22:04:04 -0600

    chapter 5.5

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/stevengpa/lets-go-pro/internal/models"
+)
+
+type templateData struct {
+	Snippet  models.Snippet
+	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// base template to template set
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// partials to template set
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// page to template set
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
