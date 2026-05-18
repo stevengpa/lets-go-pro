796111eec57ce6811c9e61a2989c0af87b0e7342
stevegpa <steven.gpa.git@gmail.com>
2026-05-17 21:27:51 -0600

    chapter 8.3

--- doc/queries.sql
+++ doc/queries.sql
@@ -46,3 +46,10 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON snippetbox.* TO 'web'@'%';
 
 FLUSH PRIVILEGES;
 
+CREATE TABLE sessions (
+                          token CHAR(43) PRIMARY KEY,
+                          data BLOB NOT NULL,
+                          expiry TIMESTAMP(6) NOT NULL
+);
+
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
